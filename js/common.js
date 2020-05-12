1537c65f9aaab254404e97ad40a78d8e05dcfef8
Albert <31998800+seuraltimez@users.noreply.github.com>
2020-05-13 00:31:53 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,12 +1,38 @@
 jQuery(function(){
-
+  initWow();
 });
+
+TweenMax.from(".logo", 1, {
+  delay: 0.4,
+  y: 10,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".nav ul li", 1, {
+  delay: 0.4,
+  opacity: 0,
+  ease: Expo.easeInOut
+}, 0.1);
+
 
 $('.text_two').delay(5000).animate({'opacity':'1'},500);
 $('.text_three').delay(10000).animate({'opacity':'1'},500);
 
-
-
+function initWow(){
+  var wow = new WOW(
+  {
+    boxClass:     'wow',      
+    animateClass: 'animated', 
+    offset:       0,          
+    mobile:       true,       
+    live:         true,       
+    callback:     function(box) {},
+    scrollContainer: null
+  }
+  );
+  wow.init();
+}
 
 // ==================================================
 // fancyBox v3.3.5
