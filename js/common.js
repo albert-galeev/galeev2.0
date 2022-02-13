04ac4b03b16bf6ae0b93de0bc93048f3a521502a
Albert <albertgaleev952@gmail.com>
2022-02-13 15:36:29 +0300

    changes for effect terminal

--- js/common.js
+++ js/common.js
@@ -1,21 +1,11 @@
-paceOptions = {
-  ajax: true,
-  document: true,
-  eventLag: false
-};
-
-TweenMax.from(".logo", 1, {
-  delay: 0.4,
-  y: 10,
-  opacity: 0,
-  ease: Expo.easeInOut
-})
-
-TweenMax.staggerFrom(".nav ul li", 1, {
-  delay: 0.4,
-  opacity: 0,
-  ease: Expo.easeInOut
-}, 0.1);
-
-
-$('.text_two').delay(5000).animate({'opacity':'1'},500);
+$('.main').t({
+  mistype: 10,
+  pause_on_click: true,
+  caret: '<span style="color:hotpink;">▌</span>',
+  typing: function (elm, chr) {
+      if (chr.match(/skyblue/))
+          elm.find('.t-caret').hide();
+      else if (chr.match(/__caret/))
+          elm.find('.t-caret').show();
+  }
+});
